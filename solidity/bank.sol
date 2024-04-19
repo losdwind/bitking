df8ec547de995909700a0e5859ae6d1f0b2a63d1
vero <s524256521@gmail.com>
2024-04-19 13:14:02 +0800

    feat: add big bank smart contract

--- solidity/bank.sol
+++ solidity/bank.sol
@@ -4,20 +4,24 @@ pragma solidity ^0.8.0;
 contract Bank {
     address public owner;
     mapping(address => uint) public balances;
-    address[] public topDepositors;
+    address[3] public topDepositors;
+
+    event Withdrawed(address from, address to, uint amount);
 
     constructor() {
         owner = msg.sender;
-        topDepositors = new address ;
+        topDepositors = [address(0), address(0), address(0)] ;
     }
 
+    // 接收存款的回退函数
     receive() external payable {
         require(msg.value > 0, "Deposit amount must be greater than zero");
         balances[msg.sender] += msg.value;
         updateTopDepositors(msg.sender, balances[msg.sender]);
     }
 
-    function updateTopDepositors(address depositor, uint amount) private {
+    // 更新前三存款者的记录
+    function updateTopDepositors(address depositor, uint amount) internal {
         // Simple insertion sort
         for (uint i = 0; i < topDepositors.length; i++) {
             if (topDepositors[i] == address(0) || balances[topDepositors[i]] < amount) {
@@ -31,14 +35,19 @@ contract Bank {
         }
     }
 
+    // 提现方法，仅管理员可用
     function withdraw(uint amount) public {
         require(msg.sender == owner, "Only the owner can withdraw funds");
         require(address(this).balance >= amount, "Insufficient funds");
 
         payable(owner).transfer(amount);
+
+        emit Withdrawed(address(this), msg.sender, amount);
+
     }
 
-    function getTopDepositors() public view returns (address[] memory, uint[] memory) {
+    // 查看前三名存款者的函数
+    function getTopDepositors() public view returns (address[3] memory, uint[] memory) {
         uint[] memory topBalances = new uint[](3);
         for (uint i = 0; i < topDepositors.length; i++) {
             if (topDepositors[i] != address(0)) {
@@ -48,6 +57,7 @@ contract Bank {
         return (topDepositors, topBalances);
     }
 
+    // 获取合约余额的函数
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
---
+++ solidity/big_bank.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "bank.sol";
+
+contract Ownable {
+
+    event Received(address from, address to, uint amount);
+    receive() external payable {
+        emit Received(msg.sender, address(this), msg.value); 
+    }
+
+    function withdraw(address payable bank, uint amount) public {
+        BigBank(bank).withdraw(amount);
+    }
+}
+
+contract BigBank is Bank {
+    
+    modifier minimumDeposit {
+        require (msg.value > 0.001 ether, "Deposit value must be larger than 0.001 ether");
+        _;
+    }
+
+    receive() external payable override  minimumDeposit {
+        balances[msg.sender] += msg.value;
+        updateTopDepositors(msg.sender, balances[msg.sender]);
+    }
+
+    function delegateOwner(address newOwner) public {
+        require(msg.sender == owner, "Only owner can delegate ownership to new address");
+        owner = newOwner;
+    }
+
+}
