27a93bd3c9c44e0da4a96b20e5c923e9be499222
losdubun <s524256521@gmail.com>
2024-04-22 17:08:26 +0800

    fix: tokenbank.sol

--- solidity/erc20.sol
+++ solidity/erc20.sol
@@ -16,13 +16,13 @@ contract BaseERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) public {
+    constructor() public {
         // write your code here
         // set name,symbol,decimals,totalSupply
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply;
+        name = "BaseERC20";
+        symbol = "BERC20";
+        decimals = 18;
+        totalSupply = 100000000 ether;
 
         balances[msg.sender] = totalSupply;  
     }
@@ -35,9 +35,8 @@ contract BaseERC20 {
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         // write your code here
-        require(balances[msg.sender] >= _value, "ERC20:transfer amount exceeds balance");
-        (bool _success, ) = _to.call{value:_value}(new bytes(0));
-        require(_success, 'transfer failed');
+        require(balances[msg.sender] >= _value, "ERC20: transfer amount exceeds balance");
+
         balances[msg.sender] -= _value;
         balances[_to] += _value;
         emit Transfer(msg.sender, _to, _value);  
@@ -47,12 +46,10 @@ contract BaseERC20 {
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         // write your code here
         require(balances[_from]  >= _value, "ERC20: transfer amount exceeds balance");
-        require(allowances[_from][_to] >= _value, "RC20: transfer amount exceeds allowance");
-        (bool _success, )= _to.call{value:_value}(new bytes(0));
-        require(_success, 'error in transaction');
+        require(allowances[_from][msg.sender] >= _value, "ERC20: transfer amount exceeds allowance");
         balances[_from] -= _value;
         balances[_to] += _value;
-        allowances[_from][_to] -= _value;
+        allowances[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value); 
         return true; 
     }
---
+++ solidity/tokenbank.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract TokenBank {
+    uint public balance;
+    mapping(address => uint) public balances;
+    BaseERC20 private token;
+    address public tokenAddress;
+
+    event Deposited(address _from, address _to, uint _value);
+    event Withdrawed(address _from, address _to, uint _value);
+
+
+    constructor(address _tokenAddress){
+        token = BaseERC20(_tokenAddress);
+        tokenAddress = _tokenAddress;
+    }
+
+    function deposit(uint _value) public {
+        require(token.balanceOf(msg.sender) >= _value, "Insufficient balance");
+        // bytes memory payload = abi.encodeWithSignature("transfer(address, uint)");
+        // (bool success, ) = payable(msg.sender).delegatecall(payload);
+        require(token.allowance(msg.sender, address(this)) > _value, "Insufficient allowance");
+        // require(success, "Failed to deposit");
+        token.transferFrom(msg.sender, address(this), _value);
+        balances[msg.sender]  += _value;
+        balance += _value;
+        emit Deposited(msg.sender, address(this), _value);
+    }
+
+    function withdraw(uint _value) public{
+        require(balances[msg.sender] >= _value, "Insufficient balance");
+        require(token.balanceOf(address(this)) >= _value, "Bankcrupted");
+        token.transfer(msg.sender,_value);
+        balances[msg.sender] -= _value;
+        balance -= _value;
+    }
+
+}
+
+
+contract BaseERC20 {
+    string public name; 
+    string public symbol; 
+    uint8 public decimals; 
+
+    uint256 public totalSupply; 
+
+    mapping (address => uint256) balances; 
+
+    mapping (address => mapping (address => uint256)) allowances; 
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        // write your code here
+        // set name,symbol,decimals,totalSupply
+        name = "BaseERC20";
+        symbol = "BERC20";
+        decimals = 18;
+        totalSupply = 100000000 ether;
+
+        balances[msg.sender] = totalSupply;  
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        // write your code here
+        balance = balances[_owner];
+
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(msg.sender != _to, "Cannot transfer to self");
+        require(balances[msg.sender] >= _value, "ERC20: transfer amount exceeds balance");
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);  
+        return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(_from != _to, "Cannot transfer to same person");
+        require(balances[_from]  >= _value, "ERC20: transfer amount exceeds balance");
+        require(allowances[_from][msg.sender] >= _value, "ERC20: transfer amount exceeds allowance");
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowances[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value); 
+        return true; 
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(msg.sender != _spender, "Cannot approve yourself");
+        require(balanceOf(msg.sender) >= _value, "Insufficient balance to allow");
+
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); 
+        return true; 
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {   
+        // write your code here     
+        remaining = allowances[_owner][_spender];
+    }
+}
+
